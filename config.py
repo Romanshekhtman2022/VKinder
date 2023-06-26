9db3be5b5a4550ec5b42523c21dcca156582f9a3
Shekhtman Roman <113375484+Romanshekhtman2022@users.noreply.github.com>
2023-06-27 01:17:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
-comunity_token = 'vk1.a.cHmrxntuewcMk676eieGVxYTfm7UgyfAwdDJGcMF0vz5-5E2nF8JxyehJZ7Xi_Xwho9Ip-TXPwkp-fnbWYnB9MlzVD5W4KA88ui7VQu_mIyR4ftafo3makcWqHH4sGzmRxmsHeEmxB-MvTaLJ-N4UZzwceEhwCjA4vAtgt1uh_bdypM7UKXtljxRQ3JMmZ7QOc5Rgl_Ardd3gg9UX4o9TA'
-acces_token = 'vk1.a.MdOR1fPH7l2CEcGeKBysNGBDo75q9qwoCqWNiM9rk1wf8SjlyxUdlu1F543mNO-kfIkrRC8e26cbAlLybjCU5nbSguLuUs2x9tiDBxMnWl6MKNYS9nNKlNSjKynud9HZV2bqDitJ85iPeMRS5HTqZJv9PrDv6dp0IjHJt8LoUHM3LoetRLywIzKZZmq6l0j56aeqwdi4ut2Bd-MN6fhaXw'
+comunity_token = ''
+acces_token = ''
 db_url_object = "postgresql+psycopg2://postgres:075907590@127.0.0.1:5432/Vkinder"
 
 
